132940b490f3bbd862d623f313285774a9ef9848
mostafaateya <mostafaateya@gmail.com>
2023-10-04 03:34:19 -0700

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+"""Initial comment for code"""
+
 
 class square():
+"""square"""
     
     width = 0
     height = 0
-
     
     def __init__(self, *args, **kwargs):
+	"""__init__ func"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
+	"""Perimter of square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+	"""Convert to string func"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
